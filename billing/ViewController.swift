61c1fb8e6e5d4182ba5f59dc02e6739fa95532b0
TylerLyczak <tyler.m.lyczak@gmail.com>
2020-06-02 14:23:37 -0500

    Initial Commit

---
+++ billing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  billing
+//
+//  Created by Tyler Lyczak on 6/2/20.
+//  Copyright © 2020 Tyler Lyczak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
